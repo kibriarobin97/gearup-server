12f87e5fe732f9602e096f7d881a8bcc9b9e38ff
Golam Kibria <kibriarobin97@gmail.com>
2026-07-06 18:01:08 +0600

    added express middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.APP_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Gearup server is running");
